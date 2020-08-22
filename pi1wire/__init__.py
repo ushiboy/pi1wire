48add2106b0d03e088d6904a8654f62a843543e4
ushiboy <ushiboy.dev@gmail.com>
2020-08-22 10:11:35 +0900

    add exception test

--- pi1wire/__init__.py
+++ pi1wire/__init__.py
@@ -1,3 +1,4 @@
 from ._driver import W1DriverInterface, W1Driver
+from ._exception import InvalidCRCException, UnsupportResponseException
 from ._finder import Pi1WireInterface, Pi1Wire
 from ._sensor import OneWireInterface, OneWire
