36c2cd759ffade4de1e54633771a83c4833c1b3e
ushiboy <ushiboy.dev@gmail.com>
2020-08-22 10:27:53 +0900

    add public api

--- pi1wire/__init__.py
+++ pi1wire/__init__.py
@@ -1,4 +1,6 @@
 from ._driver import W1DriverInterface, W1Driver
-from ._exception import InvalidCRCException, UnsupportResponseException
+from ._exception import InvalidCRCException, \
+        NotFoundSensorException, \
+        UnsupportResponseException
 from ._finder import Pi1WireInterface, Pi1Wire
 from ._sensor import OneWireInterface, OneWire
