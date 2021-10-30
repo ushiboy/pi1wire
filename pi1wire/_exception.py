11aff0a94778a5cafcaa0c358885c26fd47e9f7a
Anton Verburg <47820045+antonverburg@users.noreply.github.com>
2021-10-30 11:21:12 +0200

    Update _exception.py

--- pi1wire/_exception.py
+++ pi1wire/_exception.py
@@ -8,6 +8,11 @@ class NotFoundSensorException(Exception):
     Sensor Not found
     '''
 
+class PowerOnResetValueException(Exception):
+    '''
+    Power on reset value
+    '''
+
 class UnsupportResponseException(Exception):
     '''
     Unsupport Response
