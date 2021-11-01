69411f6b30366dc09bd806e19a576172695f3f1a
Anton Verburg <47820045+antonverburg@users.noreply.github.com>
2021-11-01 12:54:23 +0100

    Added  PowerOnResetValueException to definition

--- pi1wire/__init__.py
+++ pi1wire/__init__.py
@@ -1,6 +1,7 @@
 from ._driver import W1DriverInterface, W1Driver
 from ._exception import InvalidCRCException, \
         NotFoundSensorException, \
+        PowerOnResetValueException, \
         UnsupportResponseException
 from ._finder import Pi1WireInterface, Pi1Wire
 from ._sensor import OneWireInterface, OneWire
